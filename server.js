9a224733141312933ac127c93ef4111ece8de83a
Zeca Cordeiro <zecaxcr@gmail.com>
2022-08-11 21:13:13 -0300

    Frontend connection

--- server.js
+++ server.js
@@ -8,14 +8,17 @@ const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.send({
-        success: true,
-        text: "Home Page"
-    })
-})
-
 app.use('/api', api)
+
+//Frontend connection
+if ( process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
+
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
