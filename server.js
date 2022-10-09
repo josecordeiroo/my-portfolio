99d3d2c0135d682b43b5bd99ee70a897bd0f7daa
Zeca Cordeiro <zecaxcr@gmail.com>
2022-10-09 13:56:52 -0300

    heroku testing

--- server.js
+++ server.js
@@ -22,14 +22,14 @@ app.use(cors())
 app.use('/api', api)
 
 //Frontend connection
-// if ( process.env.NODE_ENV === 'production') {
-//     app.use(express.static('frontend/build'))
+if ( process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'))
 
-//     const path = require('path')
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-//     })
-// }
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
