ccaa1431433af3680d4918b461e1fcd742aac71d
Zeca Cordeiro <zecaxcr@gmail.com>
2022-10-13 19:04:10 -0300

    create a modal for projects page

--- frontend/src/components/portfolioDetail/index.js
+++ frontend/src/components/portfolioDetail/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 
-import { Title, Detail, Stats, Description, DescriptionShort, Technologies, Technology, Info, CardShaddow } from "./styles.js";
+import {
+  Title,
+  Detail,
+  Stats,
+  Description,
+  DescriptionShort,
+  Technologies,
+  Technology,
+  Info,
+  CardShaddow,
+} from "./styles.js";
+
+import { Modal } from "react-bootstrap";
 
 import { useParams, useNavigate } from "react-router-dom";
 import useApi from "../../hooks/useApi.js";
@@ -21,46 +33,62 @@ const PortfolioDetail = () => {
     }
   };
 
+  const handleClose = () => {
+    history("/portfolio");
+  };
+
   return (
-    <CardShaddow className="shaddow" onClick={exitDetailHandler}>
-      <Detail>
-        <Stats>
-          <div>
-            <Title>{data && data.data.title}</Title>
-            <DescriptionShort>
-              <p>{data && data.data.description}</p>
-            </DescriptionShort>
-          </div>
-
-          <Info>
-            <h5>Technologies Used</h5>
-
-            <Technologies>
-              {data &&
-                data.data.technologies.map((tech) => {
-                  return (
-                    <Technology key={tech.label}>
-                      <FontAwesomeIcon
-                        icon={[tech.iconType, tech.icon]}
-                        size="3x"
-                      />
-                      {tech.label}
-                    </Technology>
-                  );
-                })}
-            </Technologies>
-          </Info>
-        </Stats>
-
-        <Description>
-          <p>{data && data.data.longDescription}</p>
-          <p>See more in: <a href={`https://github.com/josecordeiroo/${data && data.data.slug}`} target="_blank" rel="noopener noreferrer">{`github.com/josecordeiroo/${data && data.data.slug}`}</a> </p>
-        </Description>
-
-        <img src={data && data.data.imgUrl} alt="illustrative"></img>
-      </Detail>
-    </CardShaddow>
+    <Modal show={true} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>{data && data.data.title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>{data && data.data.longDescription}</Modal.Body>
+      <Modal.Footer>
+        <img style={{widht:"150px", height:"150px"}} src={data && data.data.imgUrl} alt="illustrative"></img>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
 export default PortfolioDetail;
+
+//antigoooo
+
+// <CardShaddow className="shaddow" onClick={exitDetailHandler}>
+//   <Detail>
+//     <Stats>
+//       <div>
+//         <Title>{data && data.data.title}</Title>
+//         <DescriptionShort>
+//           <p>{data && data.data.description}</p>
+//         </DescriptionShort>
+//       </div>
+
+//       <Info>
+//         <h5>Technologies Used</h5>
+
+//         <Technologies>
+//           {data &&
+//             data.data.technologies.map((tech) => {
+//               return (
+//                 <Technology key={tech.label}>
+//                   <FontAwesomeIcon
+//                     icon={[tech.iconType, tech.icon]}
+//                     size="3x"
+//                   />
+//                   {tech.label}
+//                 </Technology>
+//               );
+//             })}
+//         </Technologies>
+//       </Info>
+//     </Stats>
+
+//     <Description>
+//       <p>{data && data.data.longDescription}</p>
+//       <p>See more in: <a href={`https://github.com/josecordeiroo/${data && data.data.slug}`} target="_blank" rel="noopener noreferrer">{`github.com/josecordeiroo/${data && data.data.slug}`}</a> </p>
+//     </Description>
+
+//     <img src={data && data.data.imgUrl} alt="illustrative"></img>
+//   </Detail>
+// </CardShaddow>
